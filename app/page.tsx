aa1f3c451910c6ad05a0369b2ba46346b8075eb0
Yuchi Yahagi <6488324+FlechaMaker@users.noreply.github.com>
2025-06-08 10:28:51 +0900

    アンケートリンクを設置

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ import Banner from "@/components/banner";
 export default async function Home() {
   return (
     <>
-      {/* <Banner
+      <Banner
         message="💬 ご感想お待ちしています！"
         buttonText="アンケートに回答"
         href="https://forms.gle/x5fWZB3QBcDbRHyv5"
-      /> */}
+      />
       <section className="flex flex-col items-center justify-center gap-4 py-4">
         <OverviewTable />
       </section>
